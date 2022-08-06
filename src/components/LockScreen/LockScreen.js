be2761c10f2b58674f71af43331642de931f9f3a
Wollivan <tim.smith2814@gmail.com>
2022-08-07 00:44:55 +0100

    Fix state issue

--- src/components/LockScreen/LockScreen.js
+++ src/components/LockScreen/LockScreen.js
@@ -1,5 +1,4 @@
-import React from "react";
-import { useState, useEffect } from "react/cjs/react.development";
+import React, { useState, useEffect } from "react";
 import { passcode } from "../../utils/passcode";
 import "./LockScreen.scss";
 
