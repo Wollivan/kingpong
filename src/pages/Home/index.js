9d6e35664a3d7fe705af77b45f5cdaa63ded6c2d
Tim Smith <91621088+Wollivan@users.noreply.github.com>
2022-08-07 12:59:13 +0100

    Update index.js

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -6,7 +6,18 @@ export default function Home() {
   }, []);
   return (
     <div className="center">
-      <p>welcome to kingpong</p>
+      <h3>welcome to kingpong</h3>
+
+<p>click the crown to view the leaderboard or add a player</p>
+
+<p>click the bat to add a game</p>
+
+<p>click the swords to challenge another player or check if you've been challenged6€</p>
+
+<p>click the chat bubble to talk to your fellow competitors</p>
+
+<p>each month the table will reset and we will have a new kingpong in the hall of fame</p>
+
     </div>
   );
 }
